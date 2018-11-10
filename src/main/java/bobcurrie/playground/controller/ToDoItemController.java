ee75cff69c5c821ffed3265b10fb2cbd681dd8c9
BCse1890 <bob.currie@fsresearch.co.uk>
2018-11-10 23:04:06 +0000

    Form and Post Redirect Get Pattern

--- src/main/java/bobcurrie/playground/controller/ToDoItemController.java
+++ src/main/java/bobcurrie/playground/controller/ToDoItemController.java
@@ -1,14 +1,22 @@
 package bobcurrie.playground.controller;
 
 import bobcurrie.playground.model.ToDoData;
+import bobcurrie.playground.model.ToDoItem;
 import bobcurrie.playground.service.ToDoItemService;
+import bobcurrie.playground.util.AttributeNames;
 import bobcurrie.playground.util.Mappings;
 import bobcurrie.playground.util.ViewNames;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.time.LocalDate;
+
+@Slf4j
 @Controller
 public class ToDoItemController {
 
@@ -31,5 +39,19 @@ public class ToDoItemController {
     @GetMapping(Mappings.ITEMS)
     public String items() {
         return ViewNames.ITEMS_LIST;
+    }
+
+    @GetMapping(Mappings.ADD_ITEM)
+    public String addEditItem(Model model) {
+        ToDoItem toDoItem = new ToDoItem("", "", LocalDate.now());
+        model.addAttribute(AttributeNames.TODO_ITEM, toDoItem);
+        return ViewNames.ADD_ITEM;
+    }
+
+    @PostMapping(Mappings.ADD_ITEM)
+    public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) ToDoItem toDoItem) {
+        log.info("toDoItem from  = {}", toDoItem);
+        toDoItemService.addItem(toDoItem);
+        return "redirect:/" + Mappings.ITEMS;
     }
 }
---
+++ src/main/java/bobcurrie/playground/util/AttributeNames.java
@@ -0,0 +1,10 @@
+package bobcurrie.playground.util;
+
+public class AttributeNames {
+
+    // == constants ==
+    public static final String TODO_ITEM = "toDoItem";
+
+    // == constructors ==
+    private AttributeNames() {}
+}
